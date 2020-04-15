f5ec699eab26312e7dc9a4fb1a685bdc3fd2253f
Monu Kumar <monuk7735@gmail.com>
2020-04-15 19:10:08 +0530

    Home Updated and Notice

--- app/src/main/java/com/hashbash/sangarodhak/Fragments/FragmentHome.java
+++ app/src/main/java/com/hashbash/sangarodhak/Fragments/FragmentHome.java
@@ -2,6 +2,7 @@ package com.hashbash.sangarodhak.Fragments;
 
 import android.Manifest;
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.location.Address;
 import android.location.Geocoder;
@@ -14,10 +15,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.core.content.ContextCompat;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
 import com.hashbash.sangarodhak.R;
@@ -38,6 +40,7 @@ public class FragmentHome extends Fragment implements LocationListener {
     LocationManager manager;
     Geocoder geocoder;
     List<Address> addresses;
+    private SharedPreferences preferences;
 
     public FragmentHome(Context context) {
         this.context = context;
@@ -47,6 +50,8 @@ public class FragmentHome extends Fragment implements LocationListener {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        preferences = context.getSharedPreferences(getString(R.string.pref_case_data), Context.MODE_PRIVATE);
 
         state[0] = view.findViewById(R.id.state_name);
         state[1] = view.findViewById(R.id.state_total_cases);
@@ -58,19 +63,55 @@ public class FragmentHome extends Fragment implements LocationListener {
         country[2] = view.findViewById(R.id.country_recovered);
         country[3] = view.findViewById(R.id.country_dead);
 
+        getSharedPreferenceData();
+
         manager = (LocationManager) context.getSystemService(LOCATION_SERVICE);
         geocoder = new Geocoder(context, Locale.getDefault());
 
-        if (ContextCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-            manager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 0, this);
-        }
+        if (manager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            if (ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED)
+                manager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 30 * 1000, 10, this);
+        } else if (!preferences.getString(getString(R.string.pref_case_data_state_total_cases), "hi").equals("hi"))
+            Toast.makeText(context, "Location not Enabled, Showing last saved data", Toast.LENGTH_SHORT).show();
 
         return view;
+    }
+
+    private void getSharedPreferenceData() {
+        state[0].setText(preferences.getString(getString(R.string.pref_case_data_state_name), "State"));
+        state[1].setText("Total Cases " + preferences.getString(getString(R.string.pref_case_data_state_total_cases), "Total Cases"));
+        state[2].setText("Recovered " + preferences.getString(getString(R.string.pref_case_data_state_recovered), "Recovered"));
+        state[3].setText("Dead " + preferences.getString(getString(R.string.pref_case_data_state_dead), "Dead"));
+
+        country[0].setText(preferences.getString(getString(R.string.pref_case_data_country_name), "Country"));
+        country[1].setText("Total Cases " + preferences.getString(getString(R.string.pref_case_data_country_total_cases), "Total Cases"));
+        country[2].setText("Recovered " + preferences.getString(getString(R.string.pref_case_data_country_recovered), "Recovered"));
+        country[3].setText("Dead " + preferences.getString(getString(R.string.pref_case_data_country_dead), "Dead"));
     }
 
     private void setData(String countryName, String stateName) {
         state[0].setText(stateName);
         country[0].setText(countryName);
+
+        String stateTotalCases = "", stateRecovered = "", stateDead = "", countryTotalCases = "", countryRecovered = "", countryDead = "";
+
+        state[1].setText("Total Cases " + stateTotalCases);
+        state[2].setText("Recovered " + stateRecovered);
+        state[3].setText("Dead " + stateDead);
+
+        country[1].setText("Total Cases " + countryTotalCases);
+        country[2].setText("Recovered " + countryRecovered);
+        country[3].setText("Dead " + countryDead);
+
+        preferences.edit().putString(getString(R.string.pref_case_data_state_name), stateName)
+                .putString(getString(R.string.pref_case_data_state_total_cases), stateTotalCases)
+                .putString(getString(R.string.pref_case_data_state_recovered), stateRecovered)
+                .putString(getString(R.string.pref_case_data_state_dead), stateDead)
+                .putString(getString(R.string.pref_case_data_country_name), countryName)
+                .putString(getString(R.string.pref_case_data_country_total_cases), countryTotalCases)
+                .putString(getString(R.string.pref_case_data_country_recovered), countryRecovered)
+                .putString(getString(R.string.pref_case_data_country_dead), countryDead)
+                .apply();
     }
 
     @Override
@@ -78,9 +119,11 @@ public class FragmentHome extends Fragment implements LocationListener {
         Log.d("Home", "Location Changed\n" + location);
         try {
             addresses = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
+            Log.d("Home", "Address Received\n" + addresses);
             setData(addresses.get(0).getCountryName(), addresses.get(0).getAdminArea());
         } catch (IOException e) {
             e.printStackTrace();
+            Log.d("Home", "Address Not Received");
         }
     }
 
