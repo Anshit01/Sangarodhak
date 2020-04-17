40d7d578941c7618bc406a0fb2847bb11bf017a8
Monu Kumar <monuk7735@gmail.com>
2020-04-17 11:31:16 +0530

    Notice Corrected

--- app/src/main/java/com/hashbash/sangarodhak/Fragments/FragmentNotice.java
+++ app/src/main/java/com/hashbash/sangarodhak/Fragments/FragmentNotice.java
@@ -1,5 +1,6 @@
 package com.hashbash.sangarodhak.Fragments;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -16,11 +17,16 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import com.hashbash.sangarodhak.Adapters.NoticeRecyclerAdapter;
 import com.hashbash.sangarodhak.Modals.NoticeDataModal;
 import com.hashbash.sangarodhak.R;
 
+import java.lang.reflect.Type;
 import java.util.ArrayList;
+
+import static android.content.Context.MODE_PRIVATE;
 
 public class FragmentNotice extends Fragment {
 
@@ -30,15 +36,22 @@ public class FragmentNotice extends Fragment {
 
     private ArrayList<NoticeDataModal> allNotice;
 
+    private Gson gson = new Gson();
+    private SharedPreferences statsPreference;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_notice, container, false);
 
+        statsPreference = getActivity().getSharedPreferences(getString(R.string.pref_stats_data), MODE_PRIVATE);
+
         recyclerView = view.findViewById(R.id.recycler_view);
         loading = view.findViewById(R.id.loading);
 
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        retrieveData();
         getData();
 
         return view;
@@ -50,16 +63,40 @@ public class FragmentNotice extends Fragment {
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 allNotice = new ArrayList<>();
                 for (int i = (int) dataSnapshot.getChildrenCount() - 1; i >= 0; i--)
-                    allNotice.add(new NoticeDataModal(dataSnapshot.child(""+i).child("image").toString(), dataSnapshot.child(""+i).child("from").toString(), dataSnapshot.child(""+i).child("text").toString() ));
-
-                recyclerView.setAdapter(new NoticeRecyclerAdapter(getContext(), allNotice));
-                loading.setVisibility(View.GONE);
+                    allNotice.add(new NoticeDataModal((String) dataSnapshot.child("" + i).child("image").getValue(), (String) dataSnapshot.child("" + i).child("from").getValue(), (String) dataSnapshot.child("" + i).child("text").getValue()));
+                showNotice();
             }
 
             @Override
             public void onCancelled(@NonNull DatabaseError databaseError) {
             }
         });
+    }
+
+    private void showNotice() {
+        loading.setVisibility(View.GONE);
+        recyclerView.setAdapter(new NoticeRecyclerAdapter(getContext(), allNotice));
+        saveAllData();
+    }
+
+    private void saveAllData() {
+        String allData = gson.toJson(allNotice);
+
+        statsPreference.edit().putString(getString(R.string.pref_stats_notice_data), allData).apply();
+    }
+
+    private void retrieveData() {
+
+        String allData = statsPreference.getString(getString(R.string.pref_stats_notice_data), "[]");
+
+        if (!allData.equals("[]")) {
+            Type type = new TypeToken<ArrayList<NoticeDataModal>>() {
+            }.getType();
+
+            allNotice = gson.fromJson(allData, type);
+
+            showNotice();
+        }
 
     }
 
